f8b0a3b6e04004e8b260fc543c01e1f501ba63d7
Christopher Zhu <christopher.zhu.9@gmail.com>
2019-01-31 19:40:00 -0500

    Using snake-case since Python-infested & begin canvas drawing

--- static/public/main.js
+++ static/public/main.js
@@ -2,13 +2,37 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function mainLoop() {
+async function main_loop() {
+    const CANVAS_WIDTH = 800;
+    const CANVAS_HEIGHT = 600;
+
+    const CHAR_RADIUS = 25;
+
+    var canvas = document.getElementById('canvas');
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+
+    // Draw a 'player'
+    // should be a circle, not rectangle
+    var context = canvas.getContext('2d');
+    context.fillStyle = 'red';
+    context.fillRect(
+        (CANVAS_WIDTH/2) - (CHAR_RADIUS),
+        (CANVAS_HEIGHT/2) - (CHAR_RADIUS),
+        CHAR_RADIUS*2,
+        CHAR_RADIUS*2
+    );
+
     /* Const for reference in calculations
     *  - current desired framerate is ~30 fps
     */  
-    const desiredFramerateMs = 33;
-    var stateLastUTCMs;
-    var frameDelay;
+    const DESIRED_FPS = 30;
+    const FRAME_DELAY_MS = 1000/DESIRED_FPS;
+    var state_last_utc_ms;
+    var frame_delay_diff;
+
+    // FPS calculation
+    const fps_sample_num = 3;
 
     console.log("[INFO] Game loop starting");
     while (true) {
@@ -17,23 +41,24 @@ async function mainLoop() {
         // render (relevant) objects to canvas
 
         // TEMP: fake workload
-        //await sleep(Math.floor(Math.random()*desiredFramerateMs)+(desiredFramerateMs/2));
+        //await sleep(Math.floor(Math.random()*FRAME_DELAY_MS)+(FRAME_DELAY_MS/2));
 
 
-        // if (stateLastUTCMs != null)
-        if (stateLastUTCMs) {
-            frameDelay = desiredFramerateMs-(Date.now()-stateLastUTCMs);
+        // if (state_last_utc_ms != null)
+        if (state_last_utc_ms) {
+            frame_delay_diff = FRAME_DELAY_MS-(Date.now()-state_last_utc_ms);
             
-            if (frameDelay > 0) {
+            if (frame_delay_diff > 0) {
                 console.log("[DEBUG] Pausing to maintain framerate");
-                await sleep(frameDelay);
+                await sleep(frame_delay_diff);
             } else {
-                console.log("[WARN] Framerate warning: late by " + frameDelay*(-1) + "ms");
+                console.log("[WARN] Framerate warning: late by " + frame_delay_diff*(-1) + "ms");
             }
         }
 
-        stateLastUTCMs=Date.now();
+        state_last_utc_ms=Date.now();
+        console.log("Frame UTC: " + state_last_utc_ms);
     }
 }
 
-mainLoop();
+main_loop();
