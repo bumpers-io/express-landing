c934040ea81c7e8934dc1a61e2d38dde73b191b6
Christopher Zhu <christopher.zhu.9@gmail.com>
2019-05-06 11:48:17 -0400

    Move css to individual file

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+// Dependencies
 const express = require('express');
-const app = express();
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
+// Init
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+const appPort = 3000;
+
+app.set('port', appPort);
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
+// Routing
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/public/index.html'));
 });
+
+// Resources
 app.get('/static/main.js', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/public/main.js'));
 });
+app.get('/static/canvas.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/public/canvas.css'));
+});
 
-app.listen(8080, () => console.log('Express app listening on port 8080'));
+server.listen(appPort, () => {
+    console.log('HTTP server listening on port ', appPort)
+});
+
+// WebSockets
+io.on('connection', (socket) => {
+    console.log('Connected to WebSocket: ', socket);
+});
+
+// testing
+// setInterval(() => {
+//     io.sockets.emit('message', 'testing');
+// }, 5000);
